15b88b86e63d342739af603819b74a37bdf063b7
Guilherme-Denarde <wanderfull.mystc@gmail.com>
2025-06-14 19:30:07 -0300

    Update on name and delete protocol

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "google_container_cluster" "app_chamada_production" {
   location = var.region
 
   remove_default_node_pool = true
-
+  initial_node_count       = 1 
   ip_allocation_policy {}
 
   networking_mode = "VPC_NATIVE"
