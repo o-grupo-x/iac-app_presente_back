ada3042598b69cfcc7ff6759c01cd95db0b49405
Guilherme-Denarde <109321031+Guilherme-Denarde@users.noreply.github.com>
2025-06-14 16:37:15 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,11 @@
-variable "project_id" {
-  description = "GCP project ID"
+variable "machine_type" {
+  description = "Type of VM for Kubernetes nodes"
   type        = string
-  default     = "app-chamada-5706"
+  default     = "e2-medium"
 }
 
-variable "region" {
-  description = "GCP region"
+variable "node_service_account" {
+  description = "Service Account for Kubernetes nodes"
   type        = string
-  default     = "us-central1"
-}
-
-variable "zone" {
-  description = "GCP zone"
-  type        = string
-  default     = "us-central1-a"
-}
-
-variable "cluster_name" {
-  description = "Name of the GKE cluster"
-  type        = string
-  default     = "frontend-cluster"
-}
-
-variable "node_count" {
-  description = "Number of nodes in the primary node pool"
-  type        = number
-  default     = 2
+  default     = "your-node-sa@app-chamada-5706.iam.gserviceaccount.com"
 }
