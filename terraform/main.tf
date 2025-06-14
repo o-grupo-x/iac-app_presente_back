893058ca7463ea9f63c0f21c2ea69dd7a3aa618e
Guilherme-Denarde <wanderfull.mystc@gmail.com>
2025-06-14 20:08:44 -0300

    Switching to auto pilot

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,13 @@ resource "google_container_cluster" "app_chamada_production" {
   location = var.region
 
   remove_default_node_pool = true
-  initial_node_count       = 1 
-  ip_allocation_policy {}
+  initial_node_count       = 1
 
-  networking_mode = "VPC_NATIVE"
   release_channel {
     channel = "REGULAR"
   }
+
+  networking_mode = "VPC_NATIVE"
 }
 
 resource "google_container_node_pool" "app_chamada_production_nodes" {
@@ -21,6 +21,7 @@ resource "google_container_node_pool" "app_chamada_production_nodes" {
     machine_type    = var.machine_type
     service_account = var.node_service_account
     disk_size_gb    = 10
+    disk_type       = "pd-standard"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
