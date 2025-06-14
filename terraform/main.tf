f513d005bedf24a54c4195389bd1d755b096a33b
Guilherme-Denarde <109321031+Guilherme-Denarde@users.noreply.github.com>
2025-06-14 16:25:43 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,15 @@
 resource "google_container_cluster" "frontend" {
   name     = var.cluster_name
-  location = var.zone
+  location = var.region
 
   remove_default_node_pool = true
   initial_node_count       = 1
-}
-
-resource "google_container_node_pool" "primary" {
-  name       = "primary"
-  location   = var.zone
-  cluster    = google_container_cluster.frontend.name
-  node_count = var.node_count
 
   node_config {
-    machine_type = "e2-medium"
+    machine_type    = var.machine_type
+    service_account = var.node_service_account  # <---- Aqui você define uma service account customizada
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
 }
