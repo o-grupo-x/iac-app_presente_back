9aa68c15bff9eaf0b8d8374b37abfd9423d028eb
Guilherme-Denarde <109321031+Guilherme-Denarde@users.noreply.github.com>
2025-06-16 12:45:46 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "google_container_cluster" "app_chamada_production" {
   location                 = var.region
 
   remove_default_node_pool = true
-  initial_node_count       = 0  # Set to 0 or remove this line
+  initial_node_count       = 1
 
   release_channel {
     channel = "REGULAR"
