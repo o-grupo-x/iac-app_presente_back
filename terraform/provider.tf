a91a78db0cd9dd109d7724ddbafd645b536bce62
Guilherme-Denarde <109321031+Guilherme-Denarde@users.noreply.github.com>
2025-06-16 13:15:32 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ provider "google" {
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
-  host                   = google_container_cluster.app_chamada_production.endpoint
+  host                   = google_container_cluster.cluster.endpoint
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.app_chamada_production.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
 }
