a58a18ca3df8049b0502d9f12ecbed222377cbcf
Guilherme-Denarde <109321031+Guilherme-Denarde@users.noreply.github.com>
2025-06-16 13:12:03 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,32 @@
-resource "google_container_node_pool" "app_chamada_production_nodes" {
+resource "google_container_cluster" "cluster" {
+  name                     = var.cluster_name
+  location                 = var.region
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  networking_mode = "VPC_NATIVE"
+
+  ip_allocation_policy {}
+
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+}
+
+resource "google_container_node_pool" "primary_nodes" {
   name       = "primary-node-pool"
-  location   = google_container_cluster.app_chamada_production.location
-  cluster    = google_container_cluster.app_chamada_production.name
+  location   = google_container_cluster.cluster.location
+  cluster    = google_container_cluster.cluster.name
 
   node_config {
     machine_type    = var.machine_type
     service_account = var.node_service_account
     disk_size_gb    = 20
     disk_type       = "pd-standard"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
   initial_node_count = 1
